8cfd1346f546b7b51a15bd367c60634534bbca9e
NazmulHasanNahid <nh.nahid4491@gmail.com>
2022-04-30 01:18:00 +0600

    mongo connect

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-const express = require('express')
-const cors = require('cors');
-const app = express()
-require('dotenv').config()
-const port = process.env.PORT || 5000
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const cors = require("cors");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
 
 //middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Were house server runnig')
-})
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zfiij.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const productCollection = client.db("warehouse").collection("products");
+    app.get("/products", async (req, res) => {
+         const query = {} 
+         const cursor = productCollection.find(query)
+         const products = await cursor.toArray()
+         res.send(products)
+    });
+  } finally {
+    //await client.close()
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Were house server runnig");
+});
 
 app.listen(port, () => {
-  console.log('listening to the port' , port)
-})
+  console.log("listening to the port", port);
+});
